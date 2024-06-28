0a2d246fa7284ee5a05b62f0cf1eb8751eb9a8f5
Márton Áron <aron17marton@gmail.com>
2024-06-28 20:40:57 +0200

    feat: add safety steps

--- blink_led.py
+++ blink_led.py
@@ -1,17 +1,34 @@
+#!/usr/bin/env python
+
 import RPi.GPIO as gpio
 import time
+from signal import signal, SIGTERM, SIGHUP
+
+GPIO_PIN_NUM: int = 21
 
 gpio.setmode(gpio.BCM)
 
-gpio.setup(21, gpio.OUT)
+gpio.setup(GPIO_PIN_NUM, gpio.OUT)
 
-try:
-    while True:
-        gpio.output(21, gpio.HIGH)
-        time.sleep(0.5)
-        gpio.output(21, gpio.LOW)
-        time.sleep(0.5)
-except KeyboardInterrupt as ex:
-    print(ex)
-finally:
-    gpio.cleanup()
+def main() -> None:
+    try:
+        signal(SIGTERM, safe_exit)
+        signal(SIGHUP, safe_exit)
+        while True:
+            gpio.output(GPIO_PIN_NUM, gpio.HIGH)
+            time.sleep(0.2)
+            gpio.output(GPIO_PIN_NUM, gpio.LOW)
+            time.sleep(0.2)
+    except KeyboardInterrupt as ex:
+        print(ex)
+    finally:
+        gpio.cleanup()
+
+
+def safe_exit(signum, frame) -> None:
+    """ Provides a safe shutdown of the program """
+    exit(1)
+
+
+if __name__ == "__main__":
+    main()
