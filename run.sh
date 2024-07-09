b3c7c12b7c554070348478b44266827bf6deeb28
Márton Áron <aron17marton@gmail.com>
2024-07-09 11:05:23 +0200

    refactoring #2

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo off
+@echo off
 /usr/bin/python /home/aron/szakdolgozat-raspberry-pi/webserver.py &
 /usr/local/bin/ngrok http --domain=hippo-immense-plainly.ngrok-free.app 8080 &
