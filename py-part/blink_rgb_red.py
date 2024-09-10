10ef6678a6f31df74022636feef16b47aafd0990
Márton Áron <aron17marton@gmail.com>
2024-09-10 17:48:46 +0200

    typos

--- py-part/blink_rgb_red.py
+++ py-part/blink_rgb_red.py
@@ -19,11 +19,8 @@ gpio.setup(GPIO_RED_PIN_NUM, gpio.OUT, initial=gpio.LOW)
 # time
 SLEEP: float = .4
 
-logger = logger.Logger("hardware-rgb-red-")
-
 
 def main() -> None:
-	logger.info("turning on/off red led")
 	try:
 		signal(SIGTERM, safe_exit)
 		signal(SIGHUP, safe_exit)
