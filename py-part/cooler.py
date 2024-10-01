893dd54e6dba1949105b573135603004f6106b8c
Márton Áron <aron17marton@gmail.com>
2024-10-01 12:42:55 +0200

    test: cooler with relay #2

--- py-part/cooler.py
+++ py-part/cooler.py
@@ -2,17 +2,16 @@
 
 import RPi.GPIO as gpio
 from signal import signal, SIGTERM, SIGHUP
-from datetime import datetime
 import time
 import os
 
 RELAY_PIN: int = 18
 
 gpio.setmode(gpio.BCM)
-gpio.setup(RELAY_PIN, gpio.OUT)
+gpio.setup(RELAY_PIN, gpio.OUT, initial=gpio.HIGH)
 
 gpio.output(RELAY_PIN, gpio.HIGH)
-time.sleep(10)
+time.sleep(5)
 gpio.output(RELAY_PIN, gpio.LOW)
 
 gpio.cleanup()
