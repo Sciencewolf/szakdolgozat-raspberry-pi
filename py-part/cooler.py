7f801759b39a4c75ffa3f58c6071205157f417f1
Márton Áron <aron17marton@gmail.com>
2024-10-01 12:56:17 +0200

    feat: cooler with relay is now working

--- py-part/cooler.py
+++ py-part/cooler.py
@@ -11,7 +11,7 @@ gpio.setmode(gpio.BCM)
 gpio.setup(RELAY_PIN, gpio.OUT, initial=gpio.HIGH)
 
 gpio.output(RELAY_PIN, gpio.HIGH)
-time.sleep(5)
+time.sleep(10)
 gpio.output(RELAY_PIN, gpio.LOW)
 
 gpio.cleanup()
