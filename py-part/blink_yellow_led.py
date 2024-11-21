6bb160da155ccf9c31988725992e9a5e7fce3a19
Márton Áron <aron17marton@gmail.com>
2024-11-21 19:02:39 +0100

    preparing files to next components

---
+++ py-part/blink_yellow_led.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+"""
+wiring:
+description:
+"""
+
+import RPi.GPIO as gpio
+import time
+from signal import signal, SIGTERM, SIGHUP
+
+# define gpio pin's
+GPIO_YELLOW_PIN_NUM: int = 20
+
+gpio.setmode(gpio.BCM)
+
+gpio.setup(GPIO_YELLOW_PIN_NUM, gpio.OUT, initial=gpio.LOW)
+
+SLEEP: float = .4
+
+
+def main() -> None:
+    try:
+        signal(SIGTERM, safe_exit)
+        signal(SIGHUP, safe_exit)
+
+        while True:
+            gpio.output(GPIO_YELLOW_PIN_NUM, gpio.HIGH)
+            time.sleep(SLEEP)
+            gpio.output(GPIO_YELLOW_PIN_NUM, gpio.LOW)
+            time.sleep(SLEEP)
+    except KeyboardInterrupt as ex:
+        pass
+    finally:
+        gpio.cleanup()
+
+
+def safe_exit(signum, frame) -> None:
+    """ Provides a safe shutdown of the program """
+    exit(1)
+
+
+if __name__ == "__main__":
+    main()
---
+++ py-part/dc_motor.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+"""
+wiring:
+description:
+"""
+
+import RPi.GPIO as gpio
+from signal import signal, SIGTERM, SIGHUP
+
+RELAY_PIN: int = -1
+
+def main() -> None:
+    try:
+        signal(SIGTERM, safe_exit)
+        signal(SIGHUP, safe_exit)
+
+        while True:
+            gpio.output(RELAY_PIN, gpio.LOW)
+    except Exception as ex:
+        print(ex.__str__())
+    finally:
+        gpio.output(RELAY_PIN, gpio.HIGH)
+        gpio.cleanup()
+
+
+def safe_exit(signum, frame) -> None:
+    """ Provides a safe shutdown of the program """
+    exit(1)
+
+
+if __name__ == "__main__":
+    main()
