755bfa0641102a51cbc7cae38818720f16565fde
Márton Áron <aron17marton@gmail.com>
2024-11-22 12:25:31 +0100

    test: logging

--- stuff/run.sh
+++ stuff/run.sh
@@ -3,4 +3,3 @@
 /usr/bin/python /home/aron/szakdolgozat-raspberry-pi/webserver.py &
 /usr/local/bin/ngrok http --domain=hippo-immense-plainly.ngrok-free.app 8080 &
 /usr/bin/python /home/aron/szakdolgozat-raspberry-pi/py-part/indicate_raspi_on.py &
-/usr/bin/git pull &
