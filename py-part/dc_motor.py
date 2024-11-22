6e3882bd6d612e636f2cf54789828ef8d64a1f28
Márton Áron <aron17marton@gmail.com>
2024-11-22 18:17:36 +0100

    feat: dc motor is ready

--- py-part/dc_motor.py
+++ py-part/dc_motor.py
@@ -7,20 +7,51 @@ description:
 
 import RPi.GPIO as gpio
 from signal import signal, SIGTERM, SIGHUP
+import time
 
-RELAY_PIN: int = -1
+ENA: int = 5
+IN1: int = 6
+IN2: int = 13
 
-def main() -> None:
+# GPIO mód kiválasztása
+gpio.setmode(gpio.BCM)
+
+# GPIO lábak beállítása kimenetként
+gpio.setup(IN1, gpio.OUT)
+gpio.setup(IN2, gpio.OUT)
+gpio.setup(ENA, gpio.OUT)
+
+# PWM inicializálása (sebességvezérléshez)
+pwm = gpio.PWM(ENA, 100)  # 100Hz PWM
+pwm.start(0)  # Sebesség 0%
+
+def main():
     try:
         signal(SIGTERM, safe_exit)
         signal(SIGHUP, safe_exit)
+        
+        # Motor előre
+        gpio.output(IN1, gpio.HIGH)
+        gpio.output(IN2, gpio.LOW)
+        pwm.ChangeDutyCycle(100)  # 100% sebesség
+        time.sleep(5)
 
-        while True:
-            gpio.output(RELAY_PIN, gpio.LOW)
-    except Exception as ex:
-        print(ex.__str__())
+        # Motor megállítása
+        gpio.output(IN1, gpio.LOW)
+        gpio.output(IN2, gpio.LOW)
+        pwm.ChangeDutyCycle(0)
+        time.sleep(2)
+
+        # Motor hátra
+        gpio.output(IN1, gpio.LOW)
+        gpio.output(IN2, gpio.HIGH)
+        pwm.ChangeDutyCycle(10)  # 10% sebesség
+        time.sleep(5)
+
+    except KeyboardInterrupt:
+        print("exiting...")
     finally:
-        gpio.output(RELAY_PIN, gpio.HIGH)
+        pwm.stop()
         gpio.cleanup()
 
 
